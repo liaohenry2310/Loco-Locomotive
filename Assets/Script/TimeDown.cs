3e02a3eb234b47f1696865f979db413df3026469
ChrityWu <wukefan233@gmail.com>
2020-05-08 15:28:57 -0700

    -added time down count

---
+++ Assets/Script/TimeDown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDown : MonoBehaviour
+{
+    public float CountDownTime;
+    private float GameTime;
+    private float timer = 0;
+    public Text GameCountTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameTime = CountDownTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int M = (int)(GameTime / 60);
+        float S = GameTime % 60;
+        //if ()//gamestart
+        //{
+            timer += Time.deltaTime;
+            if (timer >= 1f)
+            {
+                timer = 0;
+                GameTime--;
+                GameCountTimeText.text = M + ":" + string.Format("{0:00}", S);
+                if (S <= 0)
+                {
+                    GameTime = 0;
+                    //Gameover
+                }
+            }
+
+        //}
+
+    }
+}
