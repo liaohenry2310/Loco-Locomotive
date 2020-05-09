a727a7e4de005ddf6e48332fb5c115da45fbb2d4
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2020-05-08 17:19:13 -0700

    CodeReview Cyro
    - Made some test with different time float;
    - Good practice to keep name variables more reliable for better understand;
    - Changed the font size to 30 pixel for test.

--- Assets/Script/TimeDown.cs
+++ Assets/Script/TimeDown.cs
@@ -1,41 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class TimeDown : MonoBehaviour
 {
-    public float CountDownTime;
-    private float GameTime;
-    private float timer = 0;
-    public Text GameCountTimeText;
-    // Start is called before the first frame update
+    //public float CountDownTime;
+    //private float GameTime;
+    //private float timer = 0;
+    //public Text GameCountTimeText;
+
+    // Cyro review
+    [Header("Properties")]
+    [SerializeField]
+    private float TimeRemaining = 300.0f; // 5 minutes in seconds
+
+    private Text TextCountDown;
+    private bool TimerIsRunning = false;
+   
     void Start()
     {
-        GameTime = CountDownTime;
+        //GameTime = CountDownTime;
+        TextCountDown = GetComponent<Text>();
+        TimerIsRunning = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        int M = (int)(GameTime / 60);
-        float S = GameTime % 60;
-        //if ()//gamestart
+        #region Crhisty's code
+        //int M = (int)(GameTime / 60);
+        //float S = GameTime % 60;
+        ////if ()//gamestart
+        ////{
+        //timer += Time.deltaTime;
+        //if (timer >= 1f)
         //{
-            timer += Time.deltaTime;
-            if (timer >= 1f)
-            {
-                timer = 0;
-                GameTime--;
-                GameCountTimeText.text = M + ":" + string.Format("{0:00}", S);
-                if (S <= 0)
-                {
-                    GameTime = 0;
-                    //Gameover
-                }
-            }
+        //    timer = 0;
+        //    GameTime--;
+        //    GameCountTimeText.text = M + ":" + string.Format("{0:00}", S);
 
+        //    if (S <= 0)
+        //    {
+        //        GameTime = 0;
+        //        //Gameover
+        //    }
         //}
+        #endregion
 
+        if (TimerIsRunning)
+        {
+            if (TimeRemaining > 0f)
+            {
+                TimeRemaining -= Time.deltaTime;
+                DisplayTime(TimeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                TimeRemaining = 0f;
+                TimerIsRunning = false;
+            }
+        }
     }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay++;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60f);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60f);
+        TextCountDown.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
 }
