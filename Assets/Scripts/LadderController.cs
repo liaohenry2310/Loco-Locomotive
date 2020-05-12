512b1d6cf85bbfd7fcabb1303b809d14c950601c
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2020-05-11 23:46:05 -0700

    Update PlayerControllerLadder
    - Update PlayerController script, add movement right and left;
    - Add LadderController into the Ladder prefabs;
    - Update when player can climbing up or down thought the ladder.
    - Test the ladder in the main Train prefabs.

---
+++ Assets/Scripts/LadderController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LadderController : MonoBehaviour
+{
+    private PlayerController player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.LadderController = this;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.LadderController = null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private Rigidbody2D rigidBody;
+    private float inputHorizontal;
+    private float inputVertical;
+
+    public LadderController LadderController { get; set; }
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(inputHorizontal * speed, rigidBody.velocity.y);
+        if (LadderController)
+        {
+            inputVertical = Input.GetAxisRaw("Vertical");
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, inputVertical * speed);
+            if (inputVertical != 0)
+            {
+                transform.position = new Vector2(LadderController.transform.position.x, transform.position.y);
+            }
+            rigidBody.gravityScale = 0f;
+        }
+        else
+        {
+            rigidBody.gravityScale = 5f;
+        }
+    }
+
+}
