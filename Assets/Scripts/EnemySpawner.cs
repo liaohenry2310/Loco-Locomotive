46bdc598c2afa9e0968de3699c1883a0e3ecafd6
ChrityWu <wukefan233@gmail.com>
2020-05-13 21:19:59 -0700

    -added enemy spanwer
    -added enemy spanwer
    -simple test enemy spanwer

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject Enemies;
+    void Start()
+    {
+
+        InvokeRepeating("CreatEnemies", 1, 1);
+
+    }
+    public void CreatEnemies()
+    {
+        int EnemyNum;
+        EnemyNum = Random.Range(0, 3);       
+        for (int i = 0; i < EnemyNum; i++)
+        {
+            float x;
+            x = Random.Range(-6, 6);
+            Instantiate(Enemies, new Vector3(x, 2.8f, 0), Quaternion.identity);
+
+        }
+
+    }
+
+}
