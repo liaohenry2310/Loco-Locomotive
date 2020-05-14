9db9a10c9e141fef41c41fb571408ea63a44b243
Henry Liao <liaohenry2310@gmail.com>
2020-05-14 11:19:21 -0700

    Refactored code and improved Player-Ladder interaction
    - Created Player PhysicsMaterial.
    - Renamed Wagon prefabs.
    - Created Wagon_Back prefab.
    - Reworked colliders of all Wagon prefabs and removed collier gaps.
    - Set Player transform scale to {1, 1, 1}.
    - Created graphics child gameobject for Player.
    - Changed Player's collider to CapsuleCollider2D from BoxCollider2D.
    - Set train gameobject transform scale to {1, 1, 1} in ControlPlayerLadder scene.
    - Updated LadderController script to take into account passable floors and allow player to ignore floor colliders when climbing ladders.
    - Updated PlayerController script: Gravity is now a public variable, Players will now can no longer move above ladders. Refactored code so input is in Update and physics code is in FixedUpdate.
    - Added Tag called "PassableFloor".

--- Assets/Scripts/LadderController.cs
+++ Assets/Scripts/LadderController.cs
@@ -1,27 +1,62 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class LadderController : MonoBehaviour
 {
     private PlayerController player;
+    private Vector2 ladderTopPosition;
+    private List<BoxCollider2D> passbleFloors = new List<BoxCollider2D>();
 
-    private void Awake()
+    public Vector2 GetLadderTopPosition()
     {
-        player = FindObjectOfType<PlayerController>();
+        return ladderTopPosition;
+    }
+
+    private void Start()
+    {
+        Transform[] transforms = GetComponentsInChildren<Transform>();
+        foreach(Transform transform in transforms)
+        {
+            if (transform.gameObject.name == "LadderTop")
+            {
+                ladderTopPosition = transform.position;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("PassableFloor"))
         {
+            passbleFloors.Add(collision.GetComponent<BoxCollider2D>());
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.GetComponent<PlayerController>();
             player.LadderController = this;
+            foreach (BoxCollider2D floor in passbleFloors)
+            {
+                Physics2D.IgnoreCollision(player.GetComponent<CapsuleCollider2D>(), floor, true);
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("PassableFloor"))
         {
+            passbleFloors.Remove(collision.GetComponent<BoxCollider2D>());
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.GetComponent<PlayerController>();
             player.LadderController = null;
+            foreach (BoxCollider2D floor in passbleFloors)
+            {
+                Physics2D.IgnoreCollision(player.GetComponent<CapsuleCollider2D>(), floor, false);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Properties")]
     [SerializeField]
-    private float speed = 5.0f;
+    private float speed = 4.0f;
+    [SerializeField]
+    private float gravity = 5.0f;
 
     private Rigidbody2D rigidBody;
+    private float playerHeight;
     private float inputHorizontal;
     private float inputVertical;
 
@@ -16,25 +19,36 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        playerHeight = GetComponent<CapsuleCollider2D>().size.y;
     }
 
     private void Update()
     {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
         rigidBody.velocity = new Vector2(inputHorizontal * speed, rigidBody.velocity.y);
+
         if (LadderController)
         {
-            inputVertical = Input.GetAxisRaw("Vertical");
+            rigidBody.gravityScale = 0.0f;
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, inputVertical * speed);
-            if (inputVertical != 0)
+            if (inputVertical != 0.0f)
             {
-                transform.position = new Vector2(LadderController.transform.position.x, transform.position.y);
+                rigidBody.velocity = new Vector2(0.0f, rigidBody.velocity.y);
+                transform.position = new Vector2(LadderController.transform.position.x, Mathf.Min(transform.position.y, LadderController.GetLadderTopPosition().y + playerHeight * 0.5f));
             }
-            rigidBody.gravityScale = 0f;
+            else
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0.0f);
+            }
         }
         else
         {
-            rigidBody.gravityScale = 5f;
+            rigidBody.gravityScale = gravity;
         }
     }
 
