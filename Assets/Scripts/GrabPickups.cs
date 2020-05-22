871bdc60a8fa14b7c2d127ac499014b171500773
ChrityWu <wukefan233@gmail.com>
2020-05-21 21:27:43 -0700

    -fix bugs
    fix the bug with pickup
    still have problem on throw

--- Assets/Scripts/GrabPickups.cs
+++ Assets/Scripts/GrabPickups.cs
@@ -9,46 +9,52 @@ public class GrabPickups : MonoBehaviour
 {
     private InputReciever mInputReciever;
     RaycastHit2D hit;
-    public bool pickup;
+    public bool grabbed;
     public float distance = 0.5f;
     public Transform holdpoint;
-    public float throwforce=10;
-    // Start is called before the first frame update
+    public float throwforce = 10;
+    public float destorytime=10.0f;
+
     void Start()
     {
         mInputReciever = GetComponent<InputReciever>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
+
         if (mInputReciever.GetPrimaryHoldInput())
-            Debug.Log("pick up");
         {
-            if (!pickup)
+
+            if (!grabbed)
             {
                 Physics2D.queriesStartInColliders = false;
+
                 hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance);
-                if (hit.collider != null && hit.collider.tag == "Pickup")
+
+                if (hit.collider != null && hit.collider.tag == "grabbable")
                 {
-                    pickup = true;
+                    grabbed = true;
+
                 }
+
             }
-            else
+            else if (!Physics2D.OverlapPoint(holdpoint.position))
             {
-                Debug.Log("throw");
-                pickup = false;
+                grabbed = false;
+
                 if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
                 {
-                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.lossyScale.x, 1) * throwforce;
+
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+                    Destroy(this.gameObject, destorytime);
                 }
-                Destroy(this.gameObject, 10.0f);
+
             }
         }
-        if (pickup)
-            hit.collider.gameObject.transform.position = holdpoint.position;
-    }
+		if (grabbed)
+                        hit.collider.gameObject.transform.position = holdpoint.position;
+}
 
     void OnDrawGizmos()
     {
@@ -56,6 +62,9 @@ public class GrabPickups : MonoBehaviour
 
         Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x * distance);
     }
+
+
+
     //public bool canPickup = true;
     //public bool isHolding = false;
 
