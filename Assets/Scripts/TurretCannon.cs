34b5e045a5ead297c4efab385be473a153ddf484
Kairus-KWL <kev.meng.kml@gmail.com>
2020-05-22 12:35:31 -0700

    TurretCannon Updated
    - Added Value Max-Ammo .
    - Added Value Max-Health .
    - Added Value mRunOutHealth .
    - Edited Fire Function .
    - Added Function TakeDamage .
    - Added Function Repair .
    - Added Function Reload .
    - Added Function IsAlive .

--- Assets/Scripts/TurretCannon.cs
+++ Assets/Scripts/TurretCannon.cs
@@ -10,9 +10,17 @@ public class TurretCannon : MonoBehaviour
     public Transform CannonHandler;
     public Transform CannonFirePoint;
     public Text AmmoCountText;
+    public Text TurretText;
+    public Text TurretRepairText;
     public float FireRate = 100f;
     public float CannonHandlerSpeed = 10.0f;
-    public int Ammo = 10;
+    public int AmmoMax = 10;
+    private int mAmmo;
+    public float turretMaxHealth;
+    private float mTurretHealth;
+    private bool mRunOutHealth;
+
+
 
     private InputReciever mInputReciever;
     private float mTimeToFire = 0f;
@@ -20,7 +28,10 @@ public class TurretCannon : MonoBehaviour
     void Start()
     {
         mInputReciever = GetComponent<InputReciever>();
-        AmmoCountText.text = $"Ammo: {Ammo}";
+        AmmoCountText.text = $"Ammo: {mAmmo}";
+        mRunOutHealth = false;
+        mTurretHealth = turretMaxHealth;
+        mAmmo = AmmoMax;
     }
 
     void Update()
@@ -31,13 +42,63 @@ public class TurretCannon : MonoBehaviour
 
     public void Fire(bool setFire)
     {
-        if (setFire && (Ammo > 0) && (Time.time >= mTimeToFire))
+        if ( mRunOutHealth ==false && setFire && (mAmmo > 0) && (Time.time >= mTimeToFire))
         {
             mTimeToFire = Time.time + (1f / FireRate);
             var x = Instantiate(Bullets, CannonFirePoint.transform.position, Quaternion.identity);
             x.transform.rotation = CannonFirePoint.rotation;
-            AmmoCountText.text = $"Ammo: {--Ammo}";
+            AmmoCountText.text = $"Ammo: {--mAmmo}";
+        }
+        if (mAmmo==0)
+        {
+            AmmoCountText.text = $"Ammo ...... Run out ammo........!!";
         }
     }
+
+    public void TakeDamage(float takingDamage)
+    {
+        mTurretHealth -= takingDamage;
+        TurretText.text = $"! Warning !   Turret taking {takingDamage} damage ! Turret Health: {mTurretHealth}";
+        if (mTurretHealth <= 0.0f)
+        {
+            TurretText.text = $"! Turret is Out Of Order ! ";
+        }
+
+    }
+
+    public void Repair(float repairHealth)
+    {
+        mTurretHealth += repairHealth;
+        TurretRepairText.text = $"Turret repairing: {repairHealth} , Turret Health: {mTurretHealth}";
+        if (mTurretHealth>=turretMaxHealth)
+        {
+            mTurretHealth = turretMaxHealth;
+            TurretRepairText.text = $"Turret health reached Maximum ";
+        }
+    }
+
+    public void Reload(int ammo)
+    {
+        mAmmo += ammo;
+        AmmoCountText.text = $"Ammo reloading: ... Current Ammo: {mAmmo}";
+        if (mAmmo >= AmmoMax)
+        {
+            mAmmo = AmmoMax;
+            AmmoCountText.text = $"Can not Reload Anymore ... Current Ammo: {mAmmo}";
+        }
+    }
+
+    public void IsAlive()
+    {
+        if (mTurretHealth <= 0.0f)
+        {
+            mRunOutHealth = true;
+        }
+        else
+        {
+            mRunOutHealth = false;
+        }
+    }
+
 
 }
