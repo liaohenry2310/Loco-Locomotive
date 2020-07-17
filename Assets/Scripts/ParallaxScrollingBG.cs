893ae3d505a5ed19f1c9f54cccacf23dee222616
ChrityWu <wukefan233@gmail.com>
2020-07-16 22:51:09 -0700

    -parallax scrolling background
    -added background(sky and hills)
    -added parallaxScrollingBG scripts
    -added background scenes

---
+++ Assets/Scripts/ParallaxScrollingBG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrollingBG : MonoBehaviour
+{
+    [Range(1f, 40)]
+    public float scrollSpeed = 1f;
+    public float scrollOffset;
+    Vector2 startPos;
+    float newPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * -scrollSpeed, scrollOffset);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+}
