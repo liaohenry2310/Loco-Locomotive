10aa9b23571aab38d78a3974657d21a6f442cd7c
Henry Liao <liaohenry2310@gmail.com>
2020-07-23 15:33:09 -0700

    Added TitleScreen scene
    - Created TitleMenyControl script to bind level select buttons to its corresonding level.

---
+++ Assets/Scripts/TitleMenuControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleMenuControl : MonoBehaviour
+{
+    public GameObject LevelSelectPanel;
+
+    private List<Button> buttons;
+
+    private void Start()
+    {
+        buttons = new List<Button>(LevelSelectPanel.GetComponentsInChildren<Button>());
+        for (int i = 0; i < SceneManager.sceneCount && i < buttons.Count; ++i)
+        {
+            buttons[i].onClick.AddListener(() => { SceneManager.LoadScene(i + 1); });
+        }
+    }
+}
