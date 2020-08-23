be546a8a03910e239c897c9fe4acaf339e912659
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2020-08-22 18:40:54 -0700

    Update ShieldGenerator

--- Assets/Scripts/ShieldGenerator/ShieldControl.cs
+++ Assets/Scripts/ShieldGenerator/ShieldControl.cs
@@ -5,6 +5,16 @@ public class ShieldControl : MonoBehaviour
 {
     public event Action OnControllShield;
 
+    [NonSerialized] private InputReciever _inputReciever;
+
+    private void Awake()
+    {
+        if (!TryGetComponent(out _inputReciever))
+        {
+            Debug.LogWarning($"[ShiledGenerator] -- Failed to get the component: {_inputReciever.name}");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
@@ -21,11 +31,6 @@ public class ShieldControl : MonoBehaviour
             Player player = collider.GetComponent<Player>();
             //player.turretLoader = this;
         }
-    }
-
-    private void CheckIsTriggered()
-    {
-
     }
 
 }
--- Assets/Scripts/ShieldGenerator/ShieldGeneratorController.cs
+++ Assets/Scripts/ShieldGenerator/ShieldGeneratorController.cs
@@ -7,16 +7,4 @@ public class ShieldGeneratorController : MonoBehaviour
 
     [SerializeField] private ShieldControl _shieldControl;
 
-    private InputReciever _inputReciever;
-
-
-    private void Awake()
-    {
-        if (!TryGetComponent(out _inputReciever))
-        {
-            Debug.LogWarning($"[ShiledGenerator] -- Failed to get the component: {_inputReciever.name}");
-        }
-    }
-
-
 }
