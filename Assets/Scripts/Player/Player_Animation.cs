bbc971a01af2b4b97f7a4a79e02baff4dadf27fd
ChrityWu <wukefan233@gmail.com>
2020-10-30 19:46:36 -0700

    -Added annimation
    -Added annimation

---
+++ Assets/Scripts/Player/Player_Animation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Animation : MonoBehaviour
+{
+    enum State
+    {
+        Idle = 0,
+        Move = 1,
+        IdleAndHolding = 2,
+        MovingAndHolding = 3,
+        Climbing = 4,
+        ClimbingAndHolding =5,
+        UsingTurretBegin =6,
+        UsingTurret =7
+    }
+    private GameObject player;
+    public Animator animator;
+
+
+}
